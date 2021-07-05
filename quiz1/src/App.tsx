f5c933a0f7b1488de3d24e573d3d631c4a5d8da6
jungai <juworaphol125@gmail.com>
2021-07-05 11:58:20 +0700

    update quiz

--- quiz1/src/App.tsx
+++ quiz1/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export type IApp = () => React.ReactElement;
 
@@ -6,37 +6,53 @@ const formatInput = (value: string) => {
   if (!value) return value;
 
   // check if string is not number reset
-  const rawValue = value.replace(/[^\d]/g, "")
+  const rawValue = value.replace(/[^\d]/g, "");
 
   // start format with length > 3
-  if (rawValue.length < 4) return rawValue
+  if (rawValue.length < 4) return rawValue;
 
   if (rawValue.length < 7) {
     return `(${rawValue.slice(0, 3)}) ${rawValue.slice(3)}`;
   }
 
-  return `(${rawValue.slice(0, 3)}) ${rawValue.slice(
-    3,
-    6
-  )}-${rawValue.slice(6, 10)}`;
-}
+  return `(${rawValue.slice(0, 3)}) ${rawValue.slice(3, 6)}-${rawValue.slice(
+    6,
+    10
+  )}`;
+};
 
 const App: IApp = () => {
-  const [inputValue, setInputValue] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>("");
+  const [list, setList] = useState<string[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setInputValue(formatInput(e.target.value))
-  }
+    setInputValue(formatInput(e.target.value));
+  };
 
   const handleClick = () => {
-    console.log(inputValue.replaceAll(/[^\d]/g, ""))
-  }
-  
+    console.log(inputValue.replaceAll(/[^\d]/g, ""));
+    setList((prev) => [...prev, inputValue.replaceAll(/[^\d]/g, "")]);
+    setInputValue("");
+  };
+
   return (
-  <div style={{ height: '100vh', display: 'grid', placeContent: 'center' }}>
-    <input type="text" value={inputValue} name="pid" id="pid" onChange={handleChange} />
-    <button onClick={handleClick}>submit</button>
-  </div>
-)}
+    <div style={{ height: "100vh", display: "grid", placeContent: "center" }}>
+      <input
+        type="text"
+        value={inputValue}
+        name="tel"
+        id="tel"
+        onChange={handleChange}
+      />
+      <button onClick={handleClick}>submit</button>
+
+      <ul>
+        {list.map((tel) => (
+          <li key={tel}>{tel}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default App;
